09b193f30096477bfb23b9cce18eba8522d8f349
Daniil Leshchev <leshev.da@gmail.com>
2025-12-15 00:06:02 +0500

    edit build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
       entry: "src/index.ts",
-      name: "KanbanWidget",
-      fileName: (format) => `kanban-widget.${format}.js`
+      formats: ["es"],
+      fileName: () => "kanban-widget.es.js"
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM"
-        }
-      }
+      external: ["react", "react-dom"]
     }
   }
 });
